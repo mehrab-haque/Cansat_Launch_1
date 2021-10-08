9c3aeaaaa77c8200560f108dfdca0b0ad4d73696
mehrab-haque <mehrab.haque.0001@gmail.com>
2021-10-08 01:27:34 -0700

    initialized eject function

--- production/water_rocket/water_rocket.ino
+++ production/water_rocket/water_rocket.ino
@@ -2,6 +2,13 @@
 #include <SPI.h>
 #include <LoRa.h>
 #include <Adafruit_BMP085.h>
+
+#include <Servo.h>
+
+Servo servo;  
+int normalAngle = 0;    
+int ejectedAngle = 90;
+
 #define seaLevelPressure_hPa 1013.25
 
 Adafruit_BMP085 bmp;
@@ -9,11 +16,13 @@ Adafruit_BMP085 bmp;
 int red_light_pin= 7;
 
 float alt=0;
+
+
   
 void setup() {
   pinMode(red_light_pin, OUTPUT);
   Serial.begin(9600);
-  Serial.println("cansat initializing...");
+  Serial.println("rocket initializing...");
   if (!LoRa.begin(433E6)) 
   {
     Serial.println("Starting LoRa failed!");
@@ -24,31 +33,34 @@ void setup() {
   Serial.println("Could not find a valid BMP085 sensor, check wiring!");
   while (1) {}
   }
+  Serial.println("BMP180 initialization successfully...");
+  servo.attach(6);
+  servo.write(normalAngle);
+  Serial.println("ejection servo initialization successfully...");
+  Serial.println("cansat initialization completed...");
 }
   
 void loop() {
-
-//    float total=0;
-//
-//     for(int i=0;i<n;i++){
-//      total+=bmp.readAltitude();
-//     }
 
      alt=bmp.readAltitude();
 
      String dataString="rocket_v1.0 :: altitude = "+String(alt)+" m";
 
      Serial.println(dataString);
-    LoRa.beginPacket();
-    LoRa.print(dataString);
-    LoRa.endPacket();
-    redLight();
-    
-    delay(500);
+      LoRa.beginPacket();
+      LoRa.print(dataString);
+      LoRa.endPacket();
+      redLight();
+      
+      delay(500);
 }
 
 void redLight(){
   digitalWrite(red_light_pin, HIGH);
   delay(95);
   digitalWrite(red_light_pin, LOW);
+}
+
+void eject(){
+  servo.write(ejectedAngle);
 }
