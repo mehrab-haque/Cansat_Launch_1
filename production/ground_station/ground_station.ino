7168e45fa27ccecf5d88eddc7429d62222b8e915
mehrab-haque <mehrab.haque.0001@gmail.com>
2021-10-07 20:02:29 -0700

    ground station completed

--- production/ground_station/ground_station.ino
+++ production/ground_station/ground_station.ino
@@ -1,5 +1,5 @@
 #include <SPI.h>
-#include <LoRa.h>
+#include <LoRa.h> 
 
 void setup() 
 {
@@ -10,67 +10,25 @@ void setup()
     Serial.println("Starting LoRa failed!");
     while (1);
   }
-  Serial.println("LoRa initialized successfully...");
-//  LoRa.onReceive(onReceive);
-//  LoRa.receive();
+  Serial.println("LoRa initialized...");
+  LoRa.onReceive(onReceive);
+  LoRa.receive();
+  Serial.println("LoRa listening...");
+  Serial.println("ground station initialized...");
 }
 void loop() 
 {
-    LoRa.beginPacket();
-    LoRa.print("C0");
-    LoRa.endPacket();
-    receiveMessage(LoRa.parsePacket());
-    Serial.println("Data Sent");
-    
-    delay(2000);
-
-  
-//  if (Serial.available() > 0) {
-//    String command = Serial.readStringUntil('\n');
-//    LoRa.beginPacket();
-//    LoRa.print(command);
-//    LoRa.endPacket();
-//    Serial.print("command propagated : ");
-//    Serial.print(command);
-//    Serial.println();
-//  }
-  
-}
-
-void receiveMessage(int packetSize) {
- 
-  if (packetSize == 0) return;    
-  Serial.println("Data Received");    
-  String incoming = "";
-
-  while (LoRa.available()) {
-    incoming += (char)LoRa.read();
-  }
-  
-  if(incoming[0]!='R' and incoming[0]!='C' and incoming[0]!='B'){
-    Serial.println();
-    Serial.println("########Data Recieved########");
-    Serial.println(incoming);
-    Serial.println("########Completed########");
-    Serial.println();
-  }
 }
 
 void onReceive(int packetSize) {
-  Serial.println("Data Received");
   if (packetSize == 0) return;         
   String incoming = "";
 
   while (LoRa.available()) {
     incoming += (char)LoRa.read();
   }
-  
-  if(incoming[0]!='R' and incoming[0]!='C' and incoming[0]!='B'){
-    Serial.println();
-    Serial.println("########Data Recieved########");
-    Serial.println(incoming);
-    Serial.println("########Completed########");
-    Serial.println();
-  }
-  
+  Serial.println("########data recieved########");
+  Serial.println(incoming);
+  Serial.println("########completed########");
+  Serial.println();
 }
