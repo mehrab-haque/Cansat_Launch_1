0725b243dfcd4524d17c345a5379dce7db1fae4d
mehrab-haque <mehrab.haque.0001@gmail.com>
2021-10-08 18:01:34 +0600

    2 servos

--- functionalities/servo/servo.ino
+++ functionalities/servo/servo.ino
@@ -1,18 +1,25 @@
 #include <Servo.h>
 
-Servo servo;  
-int normalAngle = 0;    
-int ejectedAngle = 90;
+Servo servo1;  
+int normalAngle1 = 0;    
+int ejectedAngle1 = 90;
+
+Servo servo2;  
+int normalAngle2 = 0;    
+int ejectedAngle2 = 90;
 
 void setup() {
   Serial.begin(9600);
-  servo.attach(6);
+  servo1.attach(5);
+  servo2.attach(6);
 }
 
 void loop() {
   Serial.println("Hi");
-  servo.write(normalAngle);
+  servo1.write(normalAngle1);
+  servo2.write(normalAngle2);
   delay(2000);
-  servo.write(ejectedAngle);
+  servo1.write(ejectedAngle1);
+  servo2.write(ejectedAngle2);
   delay(2000);
 }
